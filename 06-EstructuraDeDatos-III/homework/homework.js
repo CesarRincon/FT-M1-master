00edc7879e9d4b9a71196bf1b7e15e6f7aab47af
CesarRincon <90701896+CesarRincon@users.noreply.github.com>
2022-07-28 14:03:35 -0500

    homework 6 y 7

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -10,8 +10,120 @@
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
+function BinarySearchTree(valor) {
+  this.value = valor;
+  this.left = null;
+  this.right = null;
+};
 
-function BinarySearchTree() {}
+
+BinarySearchTree.prototype.size = function () {
+
+  if (!this.right && !this.left) {
+    return 1
+  }
+  else if (this.left && this.right) {
+    return 1 + this.left.size() + this.right.size()
+  }
+  else if (this.left) {
+    return 1 + this.left.size();
+  }
+  else if (this.right && !this.left) {
+    return 1 + this.right.size();
+  }
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+  let hoja = new BinarySearchTree(value);
+
+  // vamos a comparar valores con la raiz
+  if (value > this.value) {
+    // derecha
+    // si es mayor entro aca (derecha)
+    if (!this.right) {
+      this.right = hoja;
+      
+    } else {
+      this.right.insert(value);
+
+    }
+  } else {
+    // izquierda
+    if (!this.left) {
+      this.left = hoja;
+    } else {
+      this.left.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function (value) {
+  if (value === this.value) return true;
+
+  if (value > this.value) {
+    if (!this.right) {
+      return false
+    } else {
+      return this.right.contains(value);
+    }
+  } else {
+    if (!this.left) {
+      return false;
+    } else {
+      return this.left.contains(value)
+    }
+  }
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function (callBack, order) {
+  if (order === "pre-order") {
+    callBack(this.value);
+    if (this.left) {
+      this.left.depthFirstForEach(callBack, order);
+    }
+
+    if (this.right) {
+      this.right.depthFirstForEach(callBack, order);
+    }
+  } else if (order === "post-order") {
+    if (this.left) {
+      this.left.depthFirstForEach(callBack, order);
+    }
+
+    if (this.right) {
+      this.right.depthFirstForEach(callBack, order);
+    }
+    callBack(this.value);
+  } else {
+    // in-order
+    if (this.left) {
+      this.left.depthFirstForEach(callBack, order);
+    }
+    callBack(this.value);
+
+    if (this.right) {
+      this.right.depthFirstForEach(callBack, order);
+    }
+  }
+};
+
+BinarySearchTree.prototype.breadthFirstForEach = function (callBack, array) {
+  if (array == null) {
+    var array = [];
+  }
+
+  if (this.left) {
+    array.push(this.left);
+  }
+
+  if (this.right) {
+    array.push(this.right);
+  }
+
+  callBack(this.value);
+
+  array.length > 0 && array.shift().breadthFirstForEach(callBack, array);
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,16 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  let arr = [1];
+  for (let i = 2; i < num + 1; i++) {
+    if (num % i === 0) {
+      arr.push(i);
+      num /= i
+      i -= 1;
+    }
+  }
+  return arr
+
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,21 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var hayaCambio = true;
+  while (hayaCambio) {
+    // voy a recorrer comparando y de ser necesario cambiar posicion
+    hayaCambio = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        // [20, 59]
+        let aux = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = aux;
+        hayaCambio = true;
+      }
+    }
+  }
+  return array
 }
 
 
@@ -24,6 +48,20 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for (let i = 1; i < array.length; i++) {
+    var x = i - 1;
+    var aux = array[i]; // 44 ejemplo
+    while (x >= 0 && array[x] > aux) {
+      array[x + 1] = array[x];
+      x = x - 1;
+    }
+    // encontre mi lugar
+    array[x + 1] = aux;
+  }
+
+  return array;
+
+
 }
 
 
@@ -32,6 +70,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    var minimo = i; // posicion del valor minimo en el arr
+    for (let x = i + 1; x < array.length; x++) {
+      if (array[x] < array[minimo]) {
+        minimo = x;
+      }
+    }
+    // salgo del for = encontré el valor minimo sin ordenar
+    if (minimo !== i) {
+      let aux = array[i];
+      array[i] = array[minimo];
+      array[minimo] = aux;
+    }
+  }
+  return array
+
 
 }
 
